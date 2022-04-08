2af00b3e9aa9caa1ed51a4f692a895fba23189c3
yogeshkumar2001 <yk9599493681@gmail.com>
2022-04-08 15:13:55 +0530

    updates

--- public/script.js
+++ public/script.js
@@ -1,30 +1,49 @@
-let sendChat = document.querySelector("#send");
-let chatMessageInput = document.querySelector("#chat");
-let chatList = document.querySelector(".chat-list");
-
-
-let username = prompt("Enter Your Name");
-console.log(username);
-
-socket.emit("join" , username);
-
-chatMessageInput.addEventListener("keyup" , function(e){
-    if(e.keyCode == 13){
-        sendChat.click();
-    }
+socket.on("add-chat" , function(chatObject){
+    // append on left
+    let chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat");
+    chatDiv.classList.add("left");
+    chatDiv.innerHTML = `${chatObject.name} : ${chatObject.chatMessage}`;
+    chatList.append(chatDiv);
+    chatList.scrollTop = chatList.scrollHeight;
 })
 
-sendChat.addEventListener("click" , function(){
-    let chat = chatMessageInput.value;
-    if(chat){
-        let chatDiv = document.createElement("div");
-        chatDiv.classList.add("chat");
-        chatDiv.classList.add("right");
-        chatDiv.innerHTML = chat;
-        chatList.append(chatDiv);
-        chatMessageInput.value = "";
-        chatList.scrollTop = chatList.scrollHeight;
 
-        socket.emit("send-chat" , chat); 
-    }
+socket.on("join-chat" , function(name){
+    let chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat");
+    chatDiv.classList.add("join");
+    chatDiv.innerHTML = `${name} joined chat !`;
+     chatList.append(chatDiv);
+    
+     let li = document.createElement("li");
+    li.innerHTML = name;
+    list.appendChild(li);    
+    chatList.scrollTop = chatList.scrollHeight;
+
 })
+// socket.on("join-list" ,function(username){
+//     let li = document.createElement("li");
+//     li.innerHTML = username;
+//      list.appendChild(li);
+// })
+
+
+socket.on("left-chat" , function(name){
+    let chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat");
+    chatDiv.classList.add("leave");
+    chatDiv.innerHTML = `${name} left chat !`;
+    chatList.append(chatDiv);
+    chatList.scrollTop = chatList.scrollHeight;
+     let liTAgs = document.querySelectorAll(".list li");
+     console.log(liTAgs);
+     for(let i =0 ; i< liTAgs.length ; i++){
+         if(liTAgs[i].outerText == name){
+             liTAgs[i].remove();
+         }
+     }
+})
+
+
+
